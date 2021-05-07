491ea69df5be5cb33cbd3cc9a650d2edcd53d14b
Sketcher-cell <45730505+Nishita-Bhagat@users.noreply.github.com>
2021-05-07 11:28:55 +0530

    Add files via upload

---
+++ Sum of elements in array.py
@@ -0,0 +1,11 @@
+# Input : arr[]=(1,2,3)
+# Output : 6
+# 1 + 2 + 3 = 6
+
+arr=[1,2,3,4,5]
+
+print(sum(arr)) # 15
+
+print(sum(arr,10)) # Adding 10 more to the previous output.
+
+print(sum(arr,-10)) # Subtracting 10 from the previous output.
